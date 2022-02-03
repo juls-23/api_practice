58b6f6e615776e76ea1076fd5032ba2e655ada5c
Julia <julia.golovko23@gmail.com>
2022-02-03 14:55:45 +0200

    Add errorHandler

--- server/app.js
+++ server/app.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
 const router  = require('./routes');
+const errorHandler = require('./middlewares/error.handler.mw');
 
 
 app.use(express.json())
 app.use('/', router)
+
+app.use(errorHandler)
 
 module.exports = app;
---
+++ server/middlewares/error.handler.mw.js
@@ -0,0 +1,7 @@
+module.exports = async (err, req, res, next) => {
+  const statusError = err.status || 500;
+  res.status(statusError).send({
+    errors: [{message: err.message || 'Internal Server Error'}]
+  })
+
+}
